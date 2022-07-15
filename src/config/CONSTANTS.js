9604b5ea612f387abc31d76a38712f3278cd45e5
Ernest Wong <36519873+ernestwong1027@users.noreply.github.com>
2022-07-15 00:13:13 -0400

    Update CONSTANTS.js

--- src/config/CONSTANTS.js
+++ src/config/CONSTANTS.js
@@ -1,4 +1,5 @@
 //Constants
 module.exports = {
+    //REPLACE DB LINK AS DB IS NO LONGER PUBLIC
     MONGOURL:'mongodb+srv://shopify:shopify@cluster1.6z8vd.mongodb.net/shopifybackendchallenge?retryWrites=true&w=majority',
 }
